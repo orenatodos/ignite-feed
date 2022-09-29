a74b203fbae2d900a9045124c90fc7f1cd89bb51
Renato Silva <orenatodos@gmail.com>
2022-09-28 22:20:57 -0300

    feat: create Sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import './assets/styles/global.css'
 
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+
+import styles from './App.module.css'
 
 export const App = () => {
-  return <Header />
+  return (
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main></main>
+      </div>
+    </>
+  )
 }
